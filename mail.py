96af5c7c9798c5c92bf4357b3c431079b05a1091
amka <amka@warungpintar.co>
2019-06-15 22:40:59 +0700

    change

--- mail.py
+++ mail.py
@@ -7,7 +7,7 @@ from email.mime.image import MIMEImage
 fromEmail = 'amkamoro2019.jtik@gmail.com'
 # You can generate an app password here to avoid storing your password in plain text
 # https://support.google.com/accounts/answer/185833?hl=en
-fromEmailPassword = 'amkamoro123!'
+fromEmailPassword = 'chfmdwrsjtqitibx'
 
 # Email you want to send the update to
 toEmail = 'amka150897@gmail.com'
